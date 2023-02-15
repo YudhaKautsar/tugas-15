29f0715ca628785f7c1a87aaa07c7cea2c563d81
Yudha Kautsar <muhammad.kautsar@iconpln.co.id>
2023-02-15 15:32:07 +0700

    first commit

---
+++ tugas-15.js
@@ -0,0 +1,8 @@
+const ab = 8
+const bc = 6
+
+const ac = Math.pow(ab,2) + Math.pow(bc,2);
+
+console.log(`Nilai AB : ${ab}`);
+console.log(`Nilai BC : ${bc}`);
+console.log(`panjang sisi AC pada segitiga siku-siku tersebut adalah : ${Math.sqrt(ac)} cm`);
